41ddc17682ec0bb7b067de39bb8f55f748e7f983
Mahmoud Yousef <Mahmoud Yousef>
2016-05-16 04:49:32 +0200

    initial commit

---
+++ app/components/app.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from "angular2/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "app/views/app.component.html"
+})
+
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+}
